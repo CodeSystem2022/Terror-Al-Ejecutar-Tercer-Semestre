45c78d0a3e9d7c03283a894cfb561371c30ccf46
Walter Diaz <whdiaz2023@gmail.com>
2023-05-05 13:48:29 -0300

    Cómo acceder a las propiedades de los Objetos

---
+++ JAVASCRIPT/Leccion04/01-04-objetos.js
@@ -0,0 +1,9 @@
+console.log(['apellido']); //Accedemos como si fuera un arreglo
+
+//for in y accedemos al objeto como si fuera un arreglo
+for(propiedad in persona){
+    console.log(propiedad);
+    console.log(propiedad[propiedad]);
+
+
+}
