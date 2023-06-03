a57911c0729407f9c6d973feec10929aaf08b988
AquilesMM <112222511+AquilesMM@users.noreply.github.com>
2023-06-03 02:20:42 -0300

    Clase 7
    Heredar métodos

--- JAVASCRIPT/Leccion05/05-03-Clases.js
+++ JAVASCRIPT/Leccion05/05-03-Clases.js
@@ -6,6 +6,7 @@
 //se recomienda que el nombre inicie con mayuscula
 class Persona {
 
+
     //creamos un constructor
     //este constructor es muy similar al constructor utilizado para crear un objeto
     //su sintaxis tambien es muy similar a la sintaxis de un metodo
@@ -16,19 +17,12 @@ class Persona {
 
 
 
+
         //CASO NO DEFINIR CONSTRUCTOR
         /*
         En el caso de que no definamos un constructor en nuestra clase
         js agregara uno vacio de manera automatica
         */
-
-        //TRABAJAR CON ATRIBUTOS STATIC
-        //es muy importante tener en cuenta que al trabajar con un atributo static, al llamarlo no se debe utilizar el operador this.
-        //ya que this apunta a la referencia de un objeto
-        //se debe apuntar a traves de la clase en si, como se ve a continuacion
-        //Persona.contadorObjetosPersona++;
-        //console.log('Se incrementa el contador, el nuevo valor es: '+Persona.contadorObjetosPersona);
-
 
         
 
@@ -62,8 +56,55 @@ class Persona {
     set apellido(apellido){
         this._apellido = apellido;
     }
+
+
+    //cabe destacar que no es necesario agregar ninguna coma entre metodo y metodo
+    //tampoco utilizamos la palabra reservada function para crear los metodos
+    //solo con el nombre js entiende que es una funcion o un metodo dentro de la clase
+
+
+    //Metodo para heredar a la clase hija
+    nombreCompleto(){
+        return this.idPersona+' '+this._nombre+ ' '+ this._apellido;
+        //Podemos observar que aunque el metodo este creado en la clase padre, este podra ser accesible
+        //desde la clase hija
+    }
+
+
+
 }
 
+
+
+
+
+
+//CREACION DE UNA NUEVA CLASE QUE EXTIENDE DE PERSONA
+
+class Empleado extends Persona{ //Clase Hija
+
+    constructor(nombre, apellido, departamento){
+        //cuando creamos un objeto de la clase hija, solo estamos inicializando el atributo de departamento
+        //pero al extender de Persona, js, no tiene forma de inicializar los atributos de nombre y apellido
+        //por lo que la unica forma que hay es llamar a su constructor para pasar los valores de nombre y apellido al constructor de la clase padre
+        //para ello lo hacemos con super, la primer linea de la clase hija es el constructor de la clase padre, y a este le pasamos los valores requeridos
+        //por lo sera necesario tomar en cuenta los atributos del constructor padre para agregarlos a los parametros, para luego pasarlos como argumetos
+        super(nombre, apellido);
+        this._departameto = departamento;
+    }
+
+    get departamento(){
+        return this._departameto;
+    }
+
+    set departamento(departamento){
+        this._departameto = departamento;
+    }
+
+    
+
+
+}
 
 
 
@@ -113,4 +154,21 @@ Cuando trabajamos con clases no existe el histing, es decir, si queremos declara
 declarado la clase, no se podra  hacer, dara un error
 
 */
+
+//CREAMOS OBJETOS DE LA CLASE HIJA
+
+let empleado1 = new Empleado('Maria', 'Gimenez', 'Sistemas');
+//al crear un objeto de la clase hija, si lo hacemos de la misma manera que lo hacemos con la clase padre
+//esto nos devolvera un error que nos dira que debemos llamar a super en el constructor
+// por lo que al crear objetos de una clase hija,es importante en el constructor de estos objetos, llamar a super 
+
+console.log(empleado1);
+console.log(empleado1.nombre);
+//en este caso vemos como se heredan los metodos de la clase padre, ya que get nombre no esta en la clase hija
+//pero si en la clase padre, y por ello podemos utilizarlo en un objeto de su clase hija
+
+
+
+//ACCEDEREMOS A METODOS DE CLASE PADRE A TRAVEZ DE OBJETO DE CLASE HIJA QUE HEREDA CLASE PADRE
+console.log(empleado1.nombreCompleto()); //Es necesario poner los parentesis para indicar que es un metodo
 
