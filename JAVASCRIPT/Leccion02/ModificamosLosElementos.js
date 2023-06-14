77c0e8f088e54a99bb19ab27872b996deee963cf
german176269 <abalosgerman@hotmail.com>
2023-06-14 20:46:42 -0300

    1.3 Modificamos los elementos

---
+++ JAVASCRIPT/Leccion02/ModificamosLosElementos.js
@@ -0,0 +1,11 @@
+//1.3 Modificamos los elementos JS (SEMANA 2)
+resultado = None
+a = '10'
+b = 0
+try:
+    resultado = a / b #modificamos
+except ZeroDivisionError as e:
+    print(f'Ocurrio un error: {e}')
+
+print(f'El resultado es: {resultado}')
+print('seguimos...')
