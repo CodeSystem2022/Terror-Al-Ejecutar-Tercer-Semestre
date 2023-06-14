7a61c5cbaa4524d4d40376f4fee5ba1dafb0f705
german176269 <abalosgerman@hotmail.com>
2023-06-14 20:37:36 -0300

    JS: 1.7 Argumentos y parámetros

---
+++ JAVASCRIPT/Leccion03/FuncionTipoExpresion.js
@@ -0,0 +1,9 @@
+//1.7 Funcion tipo expresion - JS
+
+let sumar = function(a = 4, b = 8){
+    console.log(arguments[0]); #muestra el parametro de: a
+    console.log(arguments[1]); #muestra el parametro de: b
+    return a + b + arguments[2];
+}
+resultado = sumar(3,2,9);
+console.log(resultado);
