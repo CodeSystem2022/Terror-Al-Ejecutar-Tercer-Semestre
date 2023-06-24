89345dd9e76eab152bfcd2793159296bf309903d
Ram <ramamichat@gmail.com>
2023-06-24 12:46:46 -0300

    adelantando código de semanas 4 y 5

--- JAVA/Leccion04/TestInstanceOf/TestInstanceOfRamiro.java
+++ JAVA/Leccion04/TestInstanceOf/TestInstanceOfRamiro.java
@@ -1,3 +1,5 @@
+/* 
+
 public state void determinarTipo(Empleado empleado) {
     if(empleado instanceof Gerente) {
         System.out.println("Es de tipo Gerente.");
@@ -9,5 +11,30 @@ public state void determinarTipo(Empleado empleado) {
     }
     else if(empleado instanceof Object) {
         System.out.println("Es de tipo Object.");
+    }
+}
+
+*/
+
+package test;
+
+import domain.*;
+
+public class TestInstanceOf {
+    public state void determinarTipo(Empleado empleado) {
+        if(empleado instanceof Gerente) {
+        System.out.println("Es de tipo Gerente.");
+        Gerente gerente = (Gerente) empleado;
+        // ((Gerente) empleado).getDepartamento();
+        System.out.println("Gerente = "+gerente.getDepartamento());
+        }
+        else if(empleado instanceof Empleado) {
+            System.out.println("Es de tipo Empleado.");
+        // Gerente gerente = (Gerente)empleado;
+        // System.out.println("Gerente = "+gerente.getDepartamento());
+        }
+        else if(empleado instanceof Object) {
+            System.out.println("Es de tipo Object.");
+        }
     }
 }
