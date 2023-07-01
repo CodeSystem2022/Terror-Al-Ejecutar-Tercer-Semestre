d6da3c52f3864d65ef2d4521fece9da098d45827
José Facundo Palomo <82476816+josefacundo92@users.noreply.github.com>
2023-07-01 06:46:58 -0300

    11.2 Diferentes Consultas en MySQL-> Parte 1 y 2

---
+++ JAVA/Leccion11/ListarPersonas/11.2 Consultas/consultas.sql
@@ -0,0 +1,11 @@
+--Comenzamos con CRUD: create (insertar), read (leer), update (actualizar), delete (eliminar)
+--Listar los estudiantes (read)
+SELECT * FROM estudiantes2022;
+--Insertar estudiante
+INSERT INTO estudaintes2022(nombre,apellido, telefono, emial)VALUES("Juan", "Perez", "261455456", juan@gmail.com);
+--Update (modificar)
+UPDATE estudiantes2022 SET nombre="Juan Carlos", apellido="Garcia" WHERE idestudiantes2022= 1;
+--Delate(eliminar)
+DELATE FROM estudantes2022 WHERE idestuantes2022=3;
+--para modificar el idestuantes2022 y comience en 1
+ALTER TABLE estudiantes2022 AUTO_INCREMENT = 1;
